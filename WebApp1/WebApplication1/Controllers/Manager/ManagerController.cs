804a1a94473759b29ba534a4bbac820cb2e2f72b
Влад Ерёмо <vladislaveremo@gmail.com>
2021-03-11 16:36:35 +0300

    View fixed

---
+++ WebApp1/WebApplication1/Controllers/Manager/ManagerController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers.Manager
+{
+    public class ManagerController : Controller
+    {
+        Models.TransactionContext db;
+
+        public ManagerController(Models.TransactionContext db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(db.Managers.ToList());
+        }
+    }
+}
