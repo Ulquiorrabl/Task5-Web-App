a190f339d64ede5e06d8aba2c0341db0248d72bd
Влад Ерёмо <vladislaveremo@gmail.com>
2021-03-11 17:15:35 +0300

    CRUD for product implementation

--- WebApp1/WebApplication1/Controllers/Home/HomeController.cs
+++ WebApp1/WebApplication1/Controllers/Home/HomeController.cs
@@ -29,5 +29,33 @@ namespace WebApplication1.Controllers.Home
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Delete(int id)
+        {
+            Product pr = db.Products.FirstOrDefault(x => x.ID == id);
+            db.Products.Remove(pr);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        } 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Modify(int id)
+        {
+            Product pr = db.Products.FirstOrDefault(product => product.ID == id);
+            return View(pr);
+        }
     }
 }
